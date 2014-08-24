1a8e30ebc483d58e06336231d8c1653f42c5b65b
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-08-24 13:59:00 +0200

    Simutrans SDL version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-$package = 'simutrans'
-$url = "http://sourceforge.net/projects/simutrans/files/simutrans/112-3/simuwin-sdl-112-3.zip/download"
-$file = "simuwin-sdl-112.3.zip"
-$binRoot = Get-BinRoot
+$url = "http://sourceforge.net/projects/simutrans/files/simutrans/112-3/simuwin-sdl-112-3.zip"
 
-Install-ChocolateyZipPackage $package $url $(Join-Path $binRoot "Simutrans")
-	
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+if ($is64bit) {
+	Install-ChocolateyZipPackage "simutrans" $url "${Env:ProgramFiles(x86)}"
+}else{
+	Install-ChocolateyZipPackage "simutrans" $url "${Env:ProgramFiles}"
+}
