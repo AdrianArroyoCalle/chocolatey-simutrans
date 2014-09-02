f1d4424e7a4f1d0d29bf3b72c6b662293605d6de
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-09-02 12:55:48 +0200

    Now using Helpers

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,16 @@
 $url = "http://sourceforge.net/projects/simutrans/files/simutrans/112-3/simuwin-sdl-112-3.zip"
 
-$processor = Get-WmiObject Win32_Processor
-$is64bit = $processor.AddressWidth -eq 64
+$is64bit = Get-ProcessorBits 64
 if ($is64bit) {
-	Install-ChocolateyZipPackage "simutrans" $url "${Env:ProgramFiles(x86)}"
+	$simudir = "${Env:ProgramFiles(x86)}"
 }else{
-	Install-ChocolateyZipPackage "simutrans" $url "${Env:ProgramFiles}"
+	$simudir = "${Env:ProgramFiles}"
 }
+Install-ChocolateyZipPackage "simutrans" $url "$simudir"
+Install-ChocolateyDesktopLink -TargetFilePath "$simudir\simutrans\simutrans.exe"
+
+$startMenu = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Programs))
+$simutransStartMenuGroup = Join-Path $startMenu "Simutrans"
+New-Item $simutransStartMenuGroup -type directory -force
+$startMenuLink = Join-Path $simutransStartMenuGroup "simutrans.lnk"
+Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$simudir\simutrans\simutrans.exe"
