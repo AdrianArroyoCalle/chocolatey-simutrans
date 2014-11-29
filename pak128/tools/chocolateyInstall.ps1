dfebec1cf41a91cd4f0c75aadc67bbfcc0798004
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-11-29 12:27:15 +0100

    Simutrans pak128 to 2.5.2

--- pak128/tools/chocolateyInstall.ps1
+++ pak128/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$url = "http://sourceforge.net/projects/simutrans/files/pak128/pak128%20for%20112-2/pak128-2.3.0--112.2.zip"
+$url = "http://sourceforge.net/projects/simutrans/files/pak128/pak128%20for%20RC%20120%20%282.5.2%2C%20bugfixes%29/pak128-2.5.2--RC_120.zip"
 
 $is64bit = Get-ProcessorBits 64
 if ($is64bit) {
